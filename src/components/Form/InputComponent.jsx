6dbd5b3c356b5caad179a65f242fd27ab1546043
Nestor <nestorpuentesin@gmail.com>
2023-09-28 16:09:29 -0600

    se agrego el porpTypes al compnente Form/Input

--- src/components/Form/InputComponent.jsx
+++ src/components/Form/InputComponent.jsx
@@ -1,10 +1,13 @@
-const InputComponent = ({ idName, label, type, value, placeholder, inputProps = {}, fullWidth = true, handleChange, handleBlur }) => {
+import PropTypes from "prop-types";
+import { TextField } from "@mui/material";
+
+const InputComponent = ({ idName, label, type, value, placeholder, inputProps, fullWidth, handleChange, handleBlur, error, touched }) => {
    return (
       <TextField
          id={idName}
          name={idName}
          label={label || ""}
-         type={type || "text"}
+         type={type || ""}
          fullWidth={fullWidth}
          value={value}
          placeholder={placeholder || "Ingresa tu info"}
@@ -12,8 +15,47 @@ const InputComponent = ({ idName, label, type, value, placeholder, inputProps = 
          onChange={handleChange}
          onBlur={handleBlur}
          // disabled={values.id == 0 ? false : true}
-         error={errors.username && touched.username}
-         helperText={errors.username && touched.username && errors.username}
+         error={error && touched}
+         helperText={error && touched && error}
       />
    );
 };
+
+InputComponent.PropTypes = {
+   idName: PropTypes.string.isRequired,
+   label: PropTypes.string.isRequired,
+   type: PropTypes.oneOf([
+      "text",
+      "checkbox",
+      "color",
+      "date",
+      "datetime-local",
+      "email",
+      "hidden",
+      "month",
+      "number",
+      "password",
+      "radio",
+      "range",
+      "tel",
+      "time",
+      "url",
+      "week"
+   ]),
+   value: PropTypes.isRequired,
+   placeholder: PropTypes.string,
+   inputProps: PropTypes.object,
+   fullWidth: PropTypes.oneOf([true, false]),
+   handleChange: PropTypes.func().isRequired,
+   handleBlur: PropTypes.func().isRequired,
+   error: PropTypes.any,
+   touched: PropTypes.any
+};
+
+InputComponent.defaultProps = {
+   placeholder: "",
+   inputProps: {},
+   fullWidth: true
+};
+
+export default InputComponent;
