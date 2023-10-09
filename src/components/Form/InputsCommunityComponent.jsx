b0774342558b91822087c6c574087c0e787256d1
Nestor <nestorpuentesin@gmail.com>
2023-10-09 15:23:14 -0600

    se agrego lo más actualziado de los componentes de Forms y el ejemplo de RequestBeca

--- src/components/Form/InputsCommunityComponent.jsx
+++ src/components/Form/InputsCommunityComponent.jsx
@@ -8,6 +8,38 @@ import axios from "axios";
 import { useGlobalContext } from "../../context/GlobalContext";
 import Select2Component from "./Select2Component";
 
+/** ESTRUCTURTAS PARA IMPORTAR EL COMPONENTE
+ * esta es la estructura del componente a insertar
+   <InputsCommunityComponent
+      formData={formData}
+      setFormData={setFormData}
+      values={values}
+      setValues={setValues}
+      setFieldValue={setFieldValue}
+      handleChange={handleChange}
+      handleBlur={handleBlur}
+      errors={errors}
+      touched={touched}
+   />
+
+   * esta es la estructura para la funcion getCommunity al editar (handleModify)
+   getCommunity(
+      formData.zip,
+      setFieldValue,
+      formData.community_id,
+      formData,
+      setFormData,
+      setDisabledState,
+      setDisabledCity,
+      setDisabledColony,
+      setShowLoading,
+      setDataStates,
+      setDataCities,
+      setDataColonies,
+      setDataColoniesComplete
+   );
+*/
+
 export const getCommunity = async (
    zip,
    setFieldValue,
@@ -43,6 +75,9 @@ export const getCommunity = async (
       setFieldValue("state", "Selecciona una opción...");
       setFieldValue("city", "Selecciona una opción...");
       setFieldValue("colony", "Selecciona una opción...");
+      setFieldValue("street", formData.street);
+      setFieldValue("num_ext", formData.num_ext);
+      setFieldValue("num_int", formData.num_int);
       if (community_id) {
          const axiosMyCommunity = axios;
          const { data } = await axiosMyCommunity.get(`https://api.gomezpalacio.gob.mx/api/cp/colonia/${community_id}`);
@@ -56,27 +91,29 @@ export const getCommunity = async (
          await setFormData(formData);
          zip = formData.zip;
       }
-      const axiosCommunities = axios;
-      const axiosRes = await axiosCommunities.get(`https://api.gomezpalacio.gob.mx/api/cp/${zip}`);
-      if (axiosRes.data.data.status_code != 200) return Toast.Error(axiosRes.data.data.alert_text);
-      await axiosRes.data.data.result.map((d) => {
-         states.push(d.Estado);
-         cities.push(d.Municipio);
-         colonies.push(d.Colonia);
-         coloniesComplete.push({ id: d.id, label: d.Colonia });
-      });
+      if (zip.length > 1) {
+         const axiosCommunities = axios;
+         const axiosRes = await axiosCommunities.get(`https://api.gomezpalacio.gob.mx/api/cp/${zip}`);
+         if (axiosRes.data.data.status_code != 200) return Toast.Error(axiosRes.data.data.alert_text);
+         await axiosRes.data.data.result.map((d) => {
+            states.push(d.Estado);
+            cities.push(d.Municipio);
+            colonies.push(d.Colonia);
+            coloniesComplete.push({ id: d.id, label: d.Colonia });
+         });
+      }
       states = [...new Set(states)];
       cities = [...new Set(cities)];
       colonies = [...new Set(colonies)];
       coloniesComplete = [...new Set(coloniesComplete)];
 
-      if (states.length == 1) {
+      if (zip !== "" && states.length === 1) {
          setShowLoading(false);
          return Toast.Info("No hay comunidades registradas con este C.P.");
       }
       if (states.length > 2) setDisabledState(false);
       if (cities.length > 2) setDisabledCity(false);
-      if (colonies.length > 2) setDisabledColony(false);
+      if (colonies.length > 1) setDisabledColony(false);
       setDataStates(states);
       setDataCities(cities);
       setDataColonies(colonies);
@@ -85,6 +122,7 @@ export const getCommunity = async (
       setFieldValue("state", community_id ? formData.state : states.length == 1 ? states[0] : states[1]);
       setFieldValue("city", community_id ? formData.city : cities.length == 1 ? cities[0] : cities[1]);
       setFieldValue("colony", community_id ? formData.colony : colonies[0]);
+      // setFieldValue("colony", community_id ? formData.colony : colonies.length == 2 ? colonies[1] : colonies[0]);
       // setFieldValue("colony", community_id ? community_id : colonies[0]["id"]);
       setShowLoading(false);
    } catch (error) {
--- src/context/RequestBecaContext.jsx
+++ src/context/RequestBecaContext.jsx
@@ -5,255 +5,206 @@ import { CorrectRes, ErrorRes } from "../utils/Response";
 const RequestBecaContext = createContext();
 
 const formDataInitialState = {
-	id: 0,
-	tutor_id: "",
-	folio: "",
-	tutor_full_name: "",
-	tutor_phone: "",
+   id: 0,
+   tutor_id: "",
+   folio: "",
+   tutor_full_name: "",
+   tutor_phone: "",
 
-	student_data_id: 0,
-	curp: "",
-	name: "",
-	paternal_last_name: "",
-	maternal_last_name: "",
-	birthdate: "",
-	gender: "MASCULINO",
-	community_id: 0,
-	zip: "",
-	state: "",
-	city: "",
-	colony: "",
+   student_data_id: 0,
+   curp: "",
+   name: "",
+   paternal_last_name: "",
+   maternal_last_name: "",
+   birthdate: "",
+   gender: "MASCULINO",
+   community_id: 0,
+   zip: "",
+   state: "",
+   city: "",
+   colony: "",
 
-	street: "",
-	num_ext: "",
-	num_int: "",
-	disability_id: "",
+   street: "",
+   num_ext: "",
+   num_int: "",
+   disability_id: "",
+   disability: "Selecciona una opción...",
 
-	school_id: "",
-	grade: "",
-	average: "",
+   school_id: "",
+   grade: "",
+   average: ""
 };
 
-const formData1InitialState = {
-	id: 0,
-	tutor_id: "",
-	folio: "",
-	tutor_full_name: "",
-	tutor_phone: "",
-};
-const formData2InitialState = {
-	id: 0,
-	student_data_id: 0,
-	curp: "",
-	name: "",
-	paternal_last_name: "",
-	maternal_last_name: "",
-	birthdate: "",
-	gender: "MASCULINO",
-	community_id: 0,
-	zip: "",
-	state: "",
-	city: "",
-	colony: "",
-
-	street: "",
-	num_ext: "",
-	num_int: "",
-	disability_id: "",
-};
-const formData3InitialState = {
-	id: 0,
-	school_id: "",
-	grade: "",
-	average: "",
-};
 
 export default function RequestBecaContextProvider({ children }) {
-	const { auth } = useAuthContext();
-	formDataInitialState.tutor_id = auth.id;
-	formData1InitialState.tutor_id = auth.id;
-	const [formTitle, setFormTitle] = useState("REGISTRAR BECA");
-	const [textBtnSubmit, setTextBtnSumbit] = useState("AGREGAR");
-	// const [loading, setLoading] = useState(true);
-	// const [loadingAction, setLoadingAction] = useState(false);
+   const { auth } = useAuthContext();
+   formDataInitialState.tutor_id = auth.id;
+   const [formTitle, setFormTitle] = useState("REGISTRAR BECA");
+   const [textBtnSubmit, setTextBtnSumbit] = useState("AGREGAR");
+   // const [loading, setLoading] = useState(true);
+   // const [loadingAction, setLoadingAction] = useState(false);
 
-	const [requestBecas, setRequestBecas] = useState([]);
-	const [requestBeca, setRequestBeca] = useState(null);
-	const [formData, setFormData] = useState(formDataInitialState);
-	const [formData1, setFormData1] = useState(formData1InitialState);
-	const [formData2, setFormData2] = useState(formData2InitialState);
-	const [formData3, setFormData3] = useState(formData3InitialState);
-	const [openDialog, setOpenDialog] = useState(false);
+   const [requestBecas, setRequestBecas] = useState([]);
+   const [requestBeca, setRequestBeca] = useState(null);
+   const [formData, setFormData] = useState(formDataInitialState);
+   const [openDialog, setOpenDialog] = useState(false);
 
-	const toggleDrawer = (open) => (event) => {
-		try {
-			if (
-				event &&
-				event.type === "keydown" &&
-				(event.key === "Tab" || event.key === "Shift")
-			) {
-				return;
-			}
-			setOpenDialog(open);
-		} catch (error) {
-			console.log("Error en toggleDrawer:", error);
-		}
-	};
+   const toggleDrawer = (open) => (event) => {
+      try {
+         if (event && event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+            return;
+         }
+         setOpenDialog(open);
+      } catch (error) {
+         console.log("Error en toggleDrawer:", error);
+      }
+   };
 
-	const resetFormData = () => {
-		try {
-			// console.log("a resetear forms");
-			setFormData(formDataInitialState);
-			setFormData1(formData1InitialState);
-			setFormData2(formData2InitialState);
-			setFormData3(formData3InitialState);
-		} catch (error) {
-			console.log("Error en fillFormData:", error);
-		}
-	};
+   const resetFormData = () => {
+      try {
+         // console.log("a resetear forms");
+         setFormData(formDataInitialState);
+      } catch (error) {
+         console.log("Error en fillFormData:", error);
+      }
+   };
 
-	const fillFormData = (values) => {
-		try {
-			const newData = { ...formData };
-			newData.id = values.id;
-			newData.code = values.code;
-			newData.requestBeca = values.requestBeca;
-			newData.city = values.city;
-			newData.colony = values.colony;
-			newData.street = values.street;
-			newData.phone = values.phone;
-			newData.director = values.director;
-			newData.loc_for = values.loc_for;
-			newData.zone = values.zone;
-			setFormData(newData);
-		} catch (error) {
-			console.log("Error en fillFormData:", error);
-		}
-	};
+   const fillFormData = (values) => {
+      try {
+         const newData = { ...formData };
+         newData.id = values.id;
+         newData.code = values.code;
+         newData.requestBeca = values.requestBeca;
+         newData.city = values.city;
+         newData.colony = values.colony;
+         newData.street = values.street;
+         newData.phone = values.phone;
+         newData.director = values.director;
+         newData.loc_for = values.loc_for;
+         newData.zone = values.zone;
+         setFormData(newData);
+      } catch (error) {
+         console.log("Error en fillFormData:", error);
+      }
+   };
 
-	const getRequestBecas = async () => {
-		try {
-			const res = CorrectRes;
-			const axiosData = await Axios.get(`/becas`);
-			res.result.requestBecas = axiosData.data.data.result;
-			setRequestBecas(axiosData.data.data.result);
-			// console.log("requestBecas", requestBecas);
+   const getRequestBecas = async () => {
+      try {
+         const res = CorrectRes;
+         const axiosData = await Axios.get(`/becas`);
+         res.result.requestBecas = axiosData.data.data.result;
+         setRequestBecas(axiosData.data.data.result);
+         // console.log("requestBecas", requestBecas);
 
-			return res;
-		} catch (error) {
-			const res = ErrorRes;
-			console.log(error);
-			res.message = error;
-			res.alert_text = error;
-		}
-	};
+         return res;
+      } catch (error) {
+         const res = ErrorRes;
+         console.log(error);
+         res.message = error;
+         res.alert_text = error;
+      }
+   };
 
-	const showRequestBeca = async (id) => {
-		try {
-			let res = CorrectRes;
-			const axiosData = await Axios.get(`/becas/${id}`);
-			setOpenDialog(true);
-			res = axiosData.data.data;
-			// await setRequestBeca(res.result);
-			// setFormData(res.result);
-			fillFormData(res.result);
+   const showRequestBeca = async (id) => {
+      try {
+         let res = CorrectRes;
+         const axiosData = await Axios.get(`/becas/${id}`);
+         setOpenDialog(true);
+         res = axiosData.data.data;
+         // await setRequestBeca(res.result);
+         // setFormData(res.result);
+         fillFormData(res.result);
 
-			return res;
-		} catch (error) {
-			const res = ErrorRes;
-			console.log(error);
-			res.message = error;
-			res.alert_text = error;
-		}
-	};
+         return res;
+      } catch (error) {
+         const res = ErrorRes;
+         console.log(error);
+         res.message = error;
+         res.alert_text = error;
+      }
+   };
 
-	const createRequestBeca = async (requestBeca) => {
-		let res = CorrectRes;
-		try {
-			const axiosData = await Axios.post("/becas", requestBeca);
-			res = axiosData.data.data;
-			// getRequestBecas();
-			return res;
-		} catch (error) {
-			res = ErrorRes;
-			console.log(error);
-			res.message = error;
-			res.alert_text = error;
-		}
-		return res;
-	};
+   const createRequestBeca = async (requestBeca) => {
+      let res = CorrectRes;
+      try {
+         const axiosData = await Axios.post("/becas", requestBeca);
+         res = axiosData.data.data;
+         // getRequestBecas();
+         return res;
+      } catch (error) {
+         res = ErrorRes;
+         console.log(error);
+         res.message = error;
+         res.alert_text = error;
+      }
+      return res;
+   };
 
-	const updateRequestBeca = async (requestBeca) => {
-		let res = CorrectRes;
-		try {
-			const axiosData = await Axios.put("/becas", requestBeca);
-			res = axiosData.data.data;
-			console.log("el res", res);
-			// getRequestBecas();
-			return res;
-		} catch (error) {
-			res = ErrorRes;
-			console.log(error);
-			res.message = error;
-			res.alert_text = error;
-		}
-		return res;
-	};
+   const updateRequestBeca = async (requestBeca) => {
+      let res = CorrectRes;
+      try {
+         const axiosData = await Axios.put("/becas", requestBeca);
+         res = axiosData.data.data;
+         console.log("el res", res);
+         // getRequestBecas();
+         return res;
+      } catch (error) {
+         res = ErrorRes;
+         console.log(error);
+         res.message = error;
+         res.alert_text = error;
+      }
+      return res;
+   };
 
-	const deleteRequestBeca = async (id) => {
-		try {
-			let res = CorrectRes;
-			const axiosData = await Axios.delete(`/becas/${id}`);
-			// console.log("deleteRequestBeca() axiosData", axiosData.data);
-			getRequestBecas();
-			res = axiosData.data.data;
-			// console.log("res", res);
-			return res;
-		} catch (error) {
-			const res = ErrorRes;
-			console.log(error);
-			res.message = error;
-			res.alert_text = error;
-		}
-	};
+   const deleteRequestBeca = async (id) => {
+      try {
+         let res = CorrectRes;
+         const axiosData = await Axios.delete(`/becas/${id}`);
+         // console.log("deleteRequestBeca() axiosData", axiosData.data);
+         getRequestBecas();
+         res = axiosData.data.data;
+         // console.log("res", res);
+         return res;
+      } catch (error) {
+         const res = ErrorRes;
+         console.log(error);
+         res.message = error;
+         res.alert_text = error;
+      }
+   };
 
-	// useEffect(() => {
-	//    // console.log("el useEffect de RequestBecaContext");
-	//    // getRequestBecas();
-	// });
+   // useEffect(() => {
+   //    // console.log("el useEffect de RequestBecaContext");
+   //    // getRequestBecas();
+   // });
 
-	return (
-		<RequestBecaContext.Provider
-			value={{
-				formTitle,
-				setFormTitle,
-				textBtnSubmit,
-				setTextBtnSumbit,
-				requestBecas,
-				setRequestBecas,
-				requestBeca,
-				formData,
-				setFormData,
-				formData1,
-				setFormData1,
-				formData2,
-				setFormData2,
-				formData3,
-				setFormData3,
-				openDialog,
-				setOpenDialog,
-				toggleDrawer,
-				resetFormData,
-				fillFormData,
-				getRequestBecas,
-				showRequestBeca,
-				createRequestBeca,
-				updateRequestBeca,
-				deleteRequestBeca,
-			}}
-		>
-			{children}
-		</RequestBecaContext.Provider>
-	);
+   return (
+      <RequestBecaContext.Provider
+         value={{
+            formTitle,
+            setFormTitle,
+            textBtnSubmit,
+            setTextBtnSumbit,
+            requestBecas,
+            setRequestBecas,
+            requestBeca,
+            formData,
+            setFormData,
+            openDialog,
+            setOpenDialog,
+            toggleDrawer,
+            resetFormData,
+            fillFormData,
+            getRequestBecas,
+            showRequestBeca,
+            createRequestBeca,
+            updateRequestBeca,
+            deleteRequestBeca
+         }}
+      >
+         {children}
+      </RequestBecaContext.Provider>
+   );
 }
 export const useRequestBecaContext = () => useContext(RequestBecaContext);
